accf2c63a6072af0579b61823efd57523b7db9ed
Анастасия <xitrik@list.ru>
2018-02-09 13:22:42 +0300

    Добавлен тест удаления групп, создан общий класс для тестов

---
+++ addressbook-web-tests/src/test/java/ru/khitrova/addressbook/GroupDeletionTest.java
@@ -0,0 +1,17 @@
+package ru.khitrova.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest extends TestBase {
+
+    
+    @Test
+    public void testGroupDeletion() {
+
+       gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        gotoGroupPage();
+    }
+
+}
