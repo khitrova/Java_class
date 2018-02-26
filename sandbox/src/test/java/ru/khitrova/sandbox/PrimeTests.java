e592c8bcdada0878e0d0c5a84d83c9842b8279a1
Анастасия <xitrik@list.ru>
2018-02-26 15:03:03 +0300

    примеры циклов на проверке простого числа

---
+++ sandbox/src/test/java/ru/khitrova/sandbox/PrimeTests.java
@@ -0,0 +1,28 @@
+package ru.khitrova.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+    @Test
+    public void testPrimes() {
+        Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    }
+
+
+    @Test
+    public void testPrimeFast() {
+        Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+    }
+    @Test(enabled = false)
+    public void testPrimeLong() {
+        long n = Integer.MAX_VALUE;
+        Assert.assertTrue(Primes.isPrime(n));
+    }
+
+    @Test
+    public void testNonPrimes() {
+        Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE-2));
+    }
+}
