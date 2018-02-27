20bd02ea77c6422d40a98d5552291aaced32ffe8
Анастасия <xitrik@list.ru>
2018-02-27 21:05:43 +0300

    сравнение неупорядоченных коллекций в тесте модификации группы

--- addressbook-web-tests/src/test/java/ru/khitrova/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/khitrova/addressbook/model/GroupData.java
@@ -6,8 +6,17 @@ public class GroupData {
     private final String name;
     private final String header;
     private final String footer;
+    private final String id;
 
-    public GroupData(String name, String header, String footer) {
+    public GroupData(String id, String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public GroupData( String name, String header, String footer) {
+        this.id = null;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -17,8 +26,11 @@ public class GroupData {
     public String toString() {
         return "GroupData{" +
                 "name='" + name + '\'' +
+                ", id='" + id + '\'' +
                 '}';
     }
+
+    public String getId() { return id; }
 
     public String getName() {
         return name;
@@ -37,12 +49,13 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name);
+        return Objects.equals(name, groupData.name) &&
+                Objects.equals(id, groupData.id);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(name);
+        return Objects.hash(name, id);
     }
 }
