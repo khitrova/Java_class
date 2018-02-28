8a9140432e38e33053cfa4ce3a80d05bcd9c1704
Анастасия <xitrik@list.ru>
2018-02-28 16:20:21 +0300

    вычисление максимального ид через лямбда выражение

--- addressbook-web-tests/src/test/java/ru/khitrova/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/khitrova/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.khitrova.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -18,14 +19,17 @@ public class GroupCreationTest extends TestBase{
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size()+1);
 
-
+/*
         int max = 0;
         for (GroupData g :after) {
             if (g.getId()>max) {
                 max = g.getId();
             }
         }
-        group.setId(max);
+*/
+
+//вычисление максиального идентификатора
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
