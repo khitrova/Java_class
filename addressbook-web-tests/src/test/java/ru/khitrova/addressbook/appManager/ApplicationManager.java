77ceaba72dabae3b5d33a855626d0c06a7007dc6
Анастасия <xitrik@list.ru>
2018-03-23 21:21:22 +0300

    загрузка данных для проверок групп

--- addressbook-web-tests/src/test/java/ru/khitrova/addressbook/appManager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/khitrova/addressbook/appManager/ApplicationManager.java
@@ -24,6 +24,7 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
     private String browser;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser){
 
@@ -35,8 +36,10 @@ public class ApplicationManager {
 
 
     public void init() throws IOException {
+
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+        dbHelper = new DbHelper();
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         } else if (browser.equals(BrowserType.CHROME)) {
@@ -52,6 +55,7 @@ public class ApplicationManager {
         sessionHelper = new SessionHelper(wd);
         contactHelper = new ContactHelper(wd);
         sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+
     }
 
 
@@ -70,5 +74,7 @@ public class ApplicationManager {
     public ContactHelper contact() {
         return contactHelper;
     }
+
+    public DbHelper db() { return dbHelper; }
 
 }
